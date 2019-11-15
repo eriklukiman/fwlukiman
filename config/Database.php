d6a1d4e59cd2ce7f9aa73a32f85c13a479a036c4
Erik Lukiman <elukiman@traveloka.com>
2019-11-15 14:06:21 +0700

    add config for DB

---
+++ config/Database.php
@@ -0,0 +1,15 @@
+<?php
+return array (
+	'default' => array(
+		'driver'		=> 'mysql',
+		'connection'	=> array(
+			'host'		=> 'localhost',
+			'port'		=> '3306',
+			'user'		=> 'root',
+			'password'	=> '',
+			'database'	=> 'dbName',
+			'timeout'	=> 2,
+		),
+		'options'		=> array(),
+	)
+);
