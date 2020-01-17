290e2860fa6fb83a717ee0d1ed9838f4168c2627
Erik Lukiman <elukiman@traveloka.com>
2020-01-17 12:18:37 +0700

    tidying if logic

--- Cores/Exception/Base.php
+++ Cores/Exception/Base.php
@@ -27,27 +27,27 @@ class Base extends \Exception {
 		return parent::getMessage();
 	}*/
 	
-	protected static function logException() {
-		if (gettype(static::$errorCount) == "object") {
-			static::$errorCount->add(1);
-		} else {
+	protected static function logException() : void {
+		if (is_int(static::$errorCount)) {
 			static::$errorCount++;
+		} else {
+			static::$errorCount->add(1);
 		}
 	}
 	
-	public static function getStats() {
+	public static function getStats() : string {
 		return "Total exception(s): " . static::getExceptionCount() . ".";
 	}
 	
-	public static function setCountVarContainer($count) {
+	public static function setCountVarContainer($count) : void {
 		static::$errorCount =  $count;
 	}
 	
-	protected static function getExceptionCount() {
-		if (gettype(static::$errorCount) == "object") {
-			return static::$errorCount->get();
-		} else {
+	protected static function getExceptionCount() : int {
+		if (is_int(static::$errorCount)) {
 			return static::$errorCount;
+		} else {
+			return static::$errorCount->get();
 		}
 	}
 }
