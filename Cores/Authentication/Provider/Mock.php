8b52ef1cf0ae5f5f6cd2f6ea168dbd0269493aba
Erik Lukiman <elukiman@traveloka.com>
2020-05-04 10:44:52 +0700

    add mock authentication provider

---
+++ Cores/Authentication/Provider/Mock.php
@@ -0,0 +1,24 @@
+<?php
+namespace Lukiman\Cores\Authentication\Provider;
+
+use \Lukiman\Cores\Exception\Base as ExceptionBase;
+use \Lukiman\Cores\Interfaces\Authentication as IAuthentication;
+use \Lukiman\Cores\Data\Authentication as AuthData;
+
+class Mock extends Base implements IAuthentication {
+	
+	public function __construct(?array $config = null) {
+		$authData = new AuthData();
+		$authData->setUserName('test');
+		$authData->setExpired(strtotime("+1 year"));
+		$this->credentials = $authData;
+	}
+	
+	public function authWithToken(String $token) : bool {
+		return true;
+	}
+	
+	public function isAuthenticated() : bool {
+		return true;
+	}
+}
