1fa55e887fc52198d6811ca3e73a99398bcbabe7
Erik Lukiman <elukiman@traveloka.com>
2020-05-13 16:40:00 +0700

    add assertion for verifying

--- Modules/Publics/Tes.php
+++ Modules/Publics/Tes.php
@@ -1,6 +1,9 @@
 <?php
 namespace Lukiman\Modules\Publics;
 
+use Assert\Assertion;
+use Assert\AssertionFailedException;
+
 use \Lukiman\Cores\Model;
 use \Lukiman\Cores\Database;
 use \Lukiman\Cores\Database\Query as Database_Query;
@@ -180,12 +183,30 @@ class Tes extends General {
 		$role2->add($auth1)->remove($auth3)->add($auth4);
 		$a = $authL->moduleMaster();
 		var_dump($a);
-		var_dump($authL->moduleMaster()->canModify());
-		var_dump($authL->moduleMaster()->canView());
-		var_dump($authL->moduleMaster1()->canModify());
-		var_dump($authL->moduleMaster1()->canView());
-		// var_dump($authL->modulePrint_Report());
-		var_dump($authL->modulePrint_Report()->canView());
+		try {
+			// Assertion::false(true, "not false");
+			var_dump($authL->moduleMaster()->canModify());
+			Assertion::true($authL->moduleMaster()->canModify());
+			var_dump($authL->moduleMaster()->canView());
+			Assertion::false($authL->moduleMaster()->canView());
+			var_dump($authL->moduleMaster1()->canModify());
+			Assertion::false($authL->moduleMaster1()->canModify());
+			var_dump($authL->moduleMaster1()->canView());
+			Assertion::true($authL->moduleMaster1()->canView());
+			// var_dump($authL->modulePrint_Report());
+			var_dump($authL->modulePrint_Report()->canView());
+			Assertion::true($authL->modulePrint_Report()->canView());
+		}  catch(AssertionFailedException $e) {
+			// error handling
+			$det = $e->getTrace();
+			echo "\n[ERROR] " . $e->getMessage() . " In file " . $det[1]['file'] . ' line ' . $det[1]['line'] . PHP_EOL . PHP_EOL;
+			// var_dump($e->getValue());
+			// echo "\n[ERROR] File " . $det[1]['file'] . ' line ' . $det[1]['line'] . PHP_EOL . PHP_EOL;
+			return;
+			// print_r($e);
+			// $e->getValue(); // the value that caused the failure
+			// $e->getConstraints(); // the additional constraints of the assertion.
+		}
 		// $authL->remove($auth1);
 		$authL->combine($role2);
 		print_r($role2);
