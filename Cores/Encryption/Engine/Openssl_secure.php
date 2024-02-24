1409db8103e091db1a0cb559710a733b81fcf34e
eriklukiman <erik.lukiman@gmail.com>
2024-02-24 22:41:22 +0700

    add validation for encrypted length

--- Cores/Encryption/Engine/Openssl_secure.php
+++ Cores/Encryption/Engine/Openssl_secure.php
@@ -12,6 +12,8 @@ class Openssl_secure extends Openssl implements IEncryption {
     }
 
     public function decrypt (String $str) : String {
+        if (strlen($str) % 2) throw new ExceptionBase("Invalid encrypted string", 500); //string must be even
+        if (strlen($str) < 32) throw new ExceptionBase("Invalid encrypted string", 500); //string must be at least 32 characters (16 for IV and 16 for encrypted string
         $iv_len = 2 * $this->getIvLength();
         $iv = hex2bin(substr($str, 0, $iv_len));
         $str = substr($str, $iv_len);
