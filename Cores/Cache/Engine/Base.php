b6fe67b2de8242a783d41169fe470f3326797418
Cahyo Bagus L <hao.labs.2011@gmail.com>
2024-07-22 12:18:34 +0700

    feat(Cache/Engine): new method getPrefix for cache key on base class

--- Cores/Cache/Engine/Base.php
+++ Cores/Cache/Engine/Base.php
@@ -1,6 +1,7 @@
 <?php
 namespace Lukiman\Cores\Cache\Engine;
 
+use Lukiman\Cores\Loader;
 use \Lukiman\Cores\Exception\Base as ExceptionBase;
 use \Lukiman\Cores\Interfaces\Cache as ICache;
 
@@ -12,4 +13,25 @@ abstract class Base implements ICache {
 	abstract public function set(String $id, $value, ?int $ttl);
 	
 	abstract public function delete(String $id);
+
+	/**
+	 * Get prefix for Cache key
+	 *
+	 * @return string "namespace_" | "config_" 
+	 * */
+	public function getPrefix(): string {
+		// check if there is an existing config prefix then use it
+		$prefix = Loader::Config('Cache')['prefix'] ?? '';
+
+		// automatically use namespace prefix if config prefix is empty
+		if (empty($prefix) && defined('LUKIMAN_NAMESPACE_PREFIX')) {
+			$prefix = LUKIMAN_NAMESPACE_PREFIX;
+		}
+
+		if (!empty($prefix)) {
+			$prefix .= '_';
+		}
+
+		return strtolower($prefix);
+	}
 }
