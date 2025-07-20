c1046c9f89a122879c2885502ca0ff7be7dc30cd
Say Pulung <saypulung@gmail.com>
2025-07-20 11:28:41 +0800

    (fix): Delete Env.php at config directory. Reason: In the loader class, because the Env.php is exists in this directory, the environment will be enforced to be Production mode. So, the config always expecting to load {{Config}}.production.php

--- config/Env.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Lukiman\Cores\Env;
-
-// There is 2 options, PRODUCTION or STAGING
-// please make your config with suffix .production.php or .staging.php
-// Example:
-// Database.production.php or Database.staging.php
-return Env::PRODUCTION;
